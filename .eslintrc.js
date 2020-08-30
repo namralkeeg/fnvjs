d6755dc97f02a8bf44dbf594aa617e2b55fa3bc9
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2020-08-30 12:22:17 -0700

    Setup config files and libraries.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 2,
+  singleQuote: false,
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 95
+};
