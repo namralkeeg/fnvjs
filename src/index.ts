960f2b0e5ddf14cf79844f0367eff624cd34c8d7
Larry Lopez <154121+namralkeeg@users.noreply.github.com>
2020-09-02 19:56:17 -0700

    Added code docs, and changed how the library exports functions.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/**
+ * The Prime number to multiply by in the algorithm for each bit size.
+ */
 const FNV_PRIMES: { [index: number]: bigint } = {
   32: 16777619n,
   64: 1099511628211n,
@@ -7,6 +10,9 @@ const FNV_PRIMES: { [index: number]: bigint } = {
   1024: 5016456510113118655434598811035278955030765345404790744303017523831112055108147451509157692220295382716162651878526895249385292291816524375083746691371804094271873160484737966720260389217684476157468082573n
 };
 
+/**
+ * Initial hash values to use for each bit size.
+ */
 const FNV_OFFSETS: { [index: number]: bigint } = {
   32: 2166136261n,
   64: 14695981039346656037n,
@@ -16,13 +22,27 @@ const FNV_OFFSETS: { [index: number]: bigint } = {
   1024: 14197795064947621068722070641403218320880622795441933960878474914617582723252296732303717722150864096521202355549365628174669108571814760471015076148029755969804077320157692458563003215304957150157403644460363550505412711285966361610267868082893823963790439336411086884584107735010676915n
 };
 
+/**
+ * Interface for functions implementing FNV algorithms.
+ */
 interface Algorithm {
   (input: string, bits: number): bigint;
 }
 
+/**
+ * Converts UTF-8 strings to {@link Uint8Array}
+ */
 const encoder: TextEncoder = new TextEncoder();
 
-export function fnv032(input: string): number {
+/**
+ * FNV-0 32-bit specific hash algorithm. Left here for historical purposes.
+ *
+ * @param {string} input The UTF-8 string to hash.
+ * @returns {number} Hash as a 32-bit number.
+ *
+ * @deprecated Left in for historical purposes. Use FNV-1a instead.
+ */
+function fnv032(input: string): number {
   let hash = 0;
   const uint8array: Uint8Array = encoder.encode(input);
 
@@ -34,12 +54,21 @@ export function fnv032(input: string): number {
   return hash >>> 0;
 }
 
-export const fnv0: Algorithm = (input: string, bits = 32) => {
+/**
+ * FNV-0 hash algorithm. Left here for historical purposes.
+ *
+ * @param {string} input The UTF-8 string to hash.
+ * @param {number} bits Bit size to use for the hash.
+ * @returns {BigInt} Hash as a {@link bigint}.
+ *
+ * @deprecated Left in for historical purposes. Use FNV-1a instead.
+ */
+const fnv0: Algorithm = (input: string, bits = 32) => {
   const fnvPrime: bigint = FNV_PRIMES[bits];
   let hash = 0n;
   const uint8array: Uint8Array = encoder.encode(input);
 
-  for (let i = 0; i < uint8array.length; i++) {
+  for (let i = 0, c = uint8array.length; i < c; i++) {
     hash = BigInt.asUintN(bits, hash * fnvPrime);
     hash ^= BigInt(uint8array[i]);
   }
@@ -47,7 +76,13 @@ export const fnv0: Algorithm = (input: string, bits = 32) => {
   return hash;
 };
 
-export function fnv132(input: string): number {
+/**
+ * FNV-1 32-bit specific hash algorithm.
+ *
+ * @param {string} input The UTF-8 string to hash.
+ * @returns {number} Hash as a 32-bit number.
+ */
+function fnv132(input: string): number {
   let hash = 2166136261;
   const uint8array: Uint8Array = encoder.encode(input);
 
@@ -59,7 +94,14 @@ export function fnv132(input: string): number {
   return hash >>> 0;
 }
 
-export const fnv1: Algorithm = (input: string, bits = 32) => {
+/**
+ * FNV-1 hash algorithm.
+ *
+ * @param {string} input The UTF-8 string to hash.
+ * @param {number} bits Bit size to use for the hash.
+ * @returns {bigint} Hash as a {@link bigint}.
+ */
+const fnv1: Algorithm = (input: string, bits = 32) => {
   const fnvPrime: bigint = FNV_PRIMES[bits];
   let hash: bigint = FNV_OFFSETS[bits];
   const uint8array: Uint8Array = encoder.encode(input);
@@ -72,7 +114,13 @@ export const fnv1: Algorithm = (input: string, bits = 32) => {
   return hash;
 };
 
-export function fnv1a32(input: string): number {
+/**
+ * FNV-1a 32-bit specific hash algorithm.
+ *
+ * @param {number} input The UTF-8 string to hash.
+ * @returns {number} Hash as a 32-bit number.
+ */
+function fnv1a32(input: string): number {
   let hash = 2166136261;
   const uint8array: Uint8Array = encoder.encode(input);
 
@@ -84,7 +132,14 @@ export function fnv1a32(input: string): number {
   return hash >>> 0;
 }
 
-export const fnv1a: Algorithm = (input: string, bits = 32) => {
+/**
+ * FNV-1a hash algorithm.
+ *
+ * @param {string} input The UTF-8 string to hash.
+ * @param {number} bits Bit size to use for the hash.
+ * @returns {bigint} Hash as a {@link bigint}.
+ */
+const fnv1a: Algorithm = (input: string, bits = 32) => {
   const fnvPrime: bigint = FNV_PRIMES[bits];
   let hash: bigint = FNV_OFFSETS[bits];
   const uint8array: Uint8Array = encoder.encode(input);
@@ -111,6 +166,17 @@ const ALGORITHMS: { [index: string]: Algorithm } = {
   fnv1a: fnv1a
 };
 
-export default function fnv(input: string, algo: AlgorithmTypeStrings, bits = 32): bigint {
+/**
+ * The default FNV library hashing function.
+ *
+ * @param input The UTF-8 string to hash.
+ * @param algo The name of the FNV algorithm to use. (fnv0, fnv1, fnv1a)
+ * @param bits Bit size to use for the hash.
+ * @returns {bigint} Hash as a {@link bigint}.
+ */
+function fnv(input: string, algo: AlgorithmTypeStrings, bits = 32): bigint {
   return ALGORITHMS[AlgorithmType[algo]](input, bits);
 }
+
+export { fnv0, fnv032, fnv1, fnv132, fnv1a, fnv1a32, fnv };
+export default fnv;
